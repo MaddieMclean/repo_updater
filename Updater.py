9c601c60e5030f0399f06460eeb4bff6ef9f15d7
Mathew McLean <mathew.mclean@steel-eye.com>
2017-10-09 17:17:36 +0100

    Added ignore + script

---
+++ Updater.py
@@ -0,0 +1,41 @@
+import subprocess
+import os
+
+repos = {
+    'commons-sink',
+    'commons-shredder',
+    'commons-bootstrap',
+    'commons-email',
+    'commons-fix',
+    'sink-router',
+    'sink-import-handler',
+    'sink-eml-handler',
+    'sink-task-handler',
+    'sink-bootstrap-handler',
+    'sink-trayport-handler',
+    'sink-raw-handler',
+    'sink-cse-handler',
+    'sink-dlq-router',
+    'sink-csv2mail-handler'
+}
+
+
+def git(cmd):
+    return ['git'] + cmd.split()
+
+
+def clone(repo):
+    subprocess.call(git(f'clone https://github.com/steeleye/{repo}.git'))
+
+
+def pull(repo):
+    path = os.getcwd()
+    subprocess.call(git(f'-C {path}/{repo} pull'))
+
+
+for repo in repos:
+    try:
+        # clone(repo)
+        pull(repo)
+    except NotADirectoryError as e:
+        print(f'{repo} not found')
